1475dcb67d309787f286185e9f3ed5502215d65a
piebytwo <shivamsonidev@gmail.com>
2021-08-04 00:20:07 +0530

    adds new intro video

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     var vid_w_orig;
     var vid_h_orig;
     $(function() {
-        vid_w_orig = parseInt($('video').attr('width'));
-        vid_h_orig = parseInt($('video').attr('height'));
+        vid_w_orig = parseInt($('#video-viewport video').attr('width'));
+        vid_h_orig = parseInt($('#video-viewport video').attr('height'));
         $(window).resize(function () { fitVideo(); });
         $(window).trigger('resize');
     });
@@ -17,10 +17,10 @@ $(document).ready(function(){
         var scale_v = $('.fullsize-video-bg').height() / vid_h_orig;
         var scale = scale_h > scale_v ? scale_h : scale_v;
         if (scale * vid_w_orig < min_w) {scale = min_w / vid_w_orig;};
-        $('video').width(scale * vid_w_orig);
-        $('video').height(scale * vid_h_orig);
-        $('#video-viewport').scrollLeft(($('video').width() - $('.fullsize-video-bg').width()) / 2);
-        $('#video-viewport').scrollTop(($('video').height() - $('.fullsize-video-bg').height()) / 2);
+        $('#video-viewport video').width(scale * vid_w_orig);
+        $('#video-viewport video').height(scale * vid_h_orig);
+        $('#video-viewport').scrollLeft(($('#video-viewport video').width() - $('.fullsize-video-bg').width()) / 2);
+        $('#video-viewport').scrollTop(($('#video-viewport video').height() - $('.fullsize-video-bg').height()) / 2);
     };
 
 
